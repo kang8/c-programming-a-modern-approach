573f689e04327b02ac3accbdcd26c8e98d4cda30
kang <yi1115610574@gmail.com>
2022-12-04 19:49:49 +0800

    Complete Ex_2_10

---
+++ chapter_2/exercises/Ex_2_10.c
@@ -0,0 +1,10 @@
+/**
+ * In the ../dweight.c program(Section 2.4), which spaces are essential?
+ *
+ * ---
+ *
+ * 1. blank line after the derectives(2.2.1)
+ * 2. spaces after `#define`. e.g. `#define INCHES_PER_POUND 166` can't remove space to `#defineINCHES_PER_POUND166`
+ * 3. spaces around keyword. e.g. `int main` can't remove space to `intmain`, `return 0` can't remove space to `return0`
+ */
+
