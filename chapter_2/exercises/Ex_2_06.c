551052cc21db2f4e57469b9600b1df7fd59260c1
kang <yi1115610574@gmail.com>
2022-12-04 09:14:53 +0800

    Complete Ex_2_06

---
+++ chapter_2/exercises/Ex_2_06.c
@@ -0,0 +1,7 @@
+/**
+ * Why is it not a good idea for an identifier to contain more that one adjacent underscore(as in current___balance, for example)?
+ *
+ * ---
+ * 1. Hard to read, it is easy to confusion current__balance and current___balance.
+ * 2. Hard to write, it is easy to miss one or more underscores.
+ */
