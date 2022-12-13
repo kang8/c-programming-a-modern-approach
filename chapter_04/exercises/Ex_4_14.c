0fd48ac8a7aecd8620c5359f5e066afde99e0754
kang <yi1115610574@gmail.com>
2022-12-13 19:53:10 +0800

    Complete Ex_4_14

---
+++ chapter_04/exercises/Ex_4_14.c
@@ -0,0 +1,9 @@
+/**
+ * Supply parentheses to show how a C compiler would interpret each of the
+ * following expressoins.
+ *
+ * (a) a * b - c * d + e // (((a * b) - (c * d)) + e)
+ * (b) a / b % c / d     // (((a / b) % c) / d)
+ * (c) - a - b + c - + d // ((((-a) - b) + c) - (+d))
+ * (d) a * -b / c - d    // (((a * (-b)) / c) - d)
+ */
