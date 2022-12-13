78fa2020fa128697bf7b1366244387e4fd8da3b3
kang <yi1115610574@gmail.com>
2022-12-13 19:44:26 +0800

    Complete Ex_4_13

---
+++ chapter_04/exercises/Ex_4_13.c
@@ -0,0 +1,9 @@
+/**
+ * Only one of the expressions ++i and i++ is exactly the same as (i += 1);
+ * which is it? Justify your answer.
+ *
+ * ---
+ *
+ * ++i same as (i += 1).
+ * The value of both expressions is i after the increment has been performed.
+ */
