93469e83f859158a863fb338c9e9929d04d2a574
kang <yi1115610574@gmail.com>
2022-12-20 20:40:21 +0800

    Complete Ex_6_14

---
+++ chapter_06/exercises/Ex_6_14.c
@@ -0,0 +1,18 @@
+/**
+ * Find the error in the following program fragment and fix it.
+ *
+ * if (n % 2 == 0);
+ *   printf("n is even\n");
+ *
+ * --
+ *
+ * The error occur at the end of the if statement:
+ *
+ * if (n % 2 == 0);
+ *                ^--- error
+ *
+ * fix:
+ *
+ * if (n % 2 == 0)
+ *   printf("n is even\n");
+ */
