7452e4e75fbd6e474346c75a343482d67a7c4fea
kang <yi1115610574@gmail.com>
2022-12-31 09:24:22 +0800

    Complete Ex_7_02

---
+++ chapter_07/exercises/Ex_7_02.c
@@ -0,0 +1,10 @@
+/**
+ * Which of the following are not legal constants in C? Claasify each legal
+ * constant as either integer or floating-point.
+ *
+ * (a) 010E2    // legal, is floating-point, and is equality `10E2`
+ * (b) 32.1E+5  // legal, is floating-point
+ * (c) 0790     // not legal, contain invalid number `9`
+ * (d) 100_000  // not legal, contain invalid character `_`
+ * (e) 3.978e-2 // legal, is floating-point
+ */
