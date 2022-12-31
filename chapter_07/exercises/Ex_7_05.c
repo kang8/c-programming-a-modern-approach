f634d5f8b7e6a1b61f412924d13f77e0c711eb50
kang <yi1115610574@gmail.com>
2022-12-31 10:00:57 +0800

    Complete Ex_7_05

---
+++ chapter_07/exercises/Ex_7_05.c
@@ -0,0 +1,9 @@
+/**
+ * Which one of the following is not a legal way to write the number 65? (Assume
+ * that the character set is ASCII.)
+ *
+ * (a) 'A'
+ * (b) 0b1000001 // illegal, C does not have binary integer constant
+ * (c) 0101
+ * (d) 0x41
+ */
