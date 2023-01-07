911128f206d08bb1a7c0d8f23c07e3b389f3da70
kang <yi1115610574@gmail.com>
2023-01-07 15:27:16 +0800

    Complete Ex_8_02

---
+++ chapter_08/exercises/Ex_8_02.c
@@ -0,0 +1,12 @@
+/**
+ * The Q&A section shows how to use a letter as an array subscript. Describe how
+ * to use a digit (in character form) as a subscript.
+ *
+ * ---
+ *
+ * int digit[10];
+ *
+ * ch is a digit character for '9', the following equations are equal:
+ *
+ * digit[ch - '9'] == digit[9] // true
+ */
