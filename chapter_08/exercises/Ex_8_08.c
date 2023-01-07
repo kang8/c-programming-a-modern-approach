9b13316bb31c6d18dc3cfdbe36ef4767b8cc4f32
kang <yi1115610574@gmail.com>
2023-01-07 16:58:45 +0800

    Complete Ex_8_08

---
+++ chapter_08/exercises/Ex_8_08.c
@@ -0,0 +1,10 @@
+/**
+ * Write a declaration for a two-dimensional array named temperature_readings
+ * that stores one month of hourly temperature readings. (For simplicity, assume
+ * that a month has 30 days. The rows of the array should represent days of the
+ * month; the columns should represent hours of the day.)
+ *
+ * ---
+ *
+ * float temperature_readings[30][24] = {0.0f};
+ */
